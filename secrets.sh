7489271f9561b5bf9612b0acf971378370260020
meigs2 <connorradeloff1@gmail.com>
2022-12-10 14:23:53 -0600

    Fix setting session id

--- secrets.sh
+++ secrets.sh
@@ -4,6 +4,6 @@ if [ "$1" == "init" ]; then
   dotnet user-secrets init --project Nexile.Infrastructure.Tests.Integration
 elif [ "$1" == "set" ]; then
   if [ "$2" == "sessionid" ]; then
-    dotnet user-secrets set --project Nexile.Infrastructure.Tests.Integration "PathOfExileApiSessionId" "$2"
+    dotnet user-secrets set --project Nexile.Infrastructure.Tests.Integration "PathOfExileApiSessionId" "$3"
   fi
 fi
